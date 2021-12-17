fea7038452c2f0865318af2a4288874459f3f150
meegger <74745473+meegger@users.noreply.github.com>
2021-12-16 21:10:57 -0500

    first commit

---
+++ components/logo.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Logo = () => {
+    return <Link href="/">
+        <a>
+        <Image 
+            src={`/images/netflix-logo.svg`}
+            altText="Netflix Logo"
+            width={160}
+            height={60}
+        />
+        </a>
+    </Link>
+    
+} 
+export default Logo
---
+++ pages/authors/me.js
@@ -0,0 +1,4 @@
+export default function Me() {
+    const name = "Mary-Katherynnnnn"
+    return <h1>{name}</h1>
+  }
